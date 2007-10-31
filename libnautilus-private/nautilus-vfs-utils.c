f39861b8d705f8c49b402c8b9a745badab754637
Alexander Larsson <alexl@redhat.com>
2007-10-31 15:03:00 +0000

    Remove more unused gnome-vfs helpers
    2007-10-31  Alexander Larsson  <alexl@redhat.com>
    
            * libnautilus-private/nautilus-vfs-utils.[ch]:
    	Remove more unused gnome-vfs helpers
    
    
    svn path=/branches/gio-branch/; revision=13390

--- libnautilus-private/nautilus-vfs-utils.c
+++ libnautilus-private/nautilus-vfs-utils.c
@@ -6,14 +6,3 @@ gnome_vfs_error_quark (void)
 {
 	return g_quark_from_static_string ("gnome-vfs-error-quark");
 }
-
-GError *
-gnome_vfs_result_to_error (GnomeVFSResult result)
-{
-	if (result == GNOME_VFS_OK) {
-		return NULL;
-	}
-
-	return g_error_new_literal (GNOME_VFS_ERROR, result,
-				    gnome_vfs_result_to_string (result));
-}
--- libnautilus-private/nautilus-vfs-utils.h
+++ libnautilus-private/nautilus-vfs-utils.h
@@ -12,6 +12,4 @@ GQuark          gnome_vfs_error_quark      (void);
 
 #define GNOME_VFS_ERROR gnome_vfs_error_quark()
 
-GError *gnome_vfs_result_to_error (GnomeVFSResult result);
-
 #endif /* __NAUTILUS_VFS_UTILS_H__ */
